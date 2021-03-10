3ed58f1677951083c7f125db89cf85e79edc5567
ryan <21905274@etu.unicaen.fr>
2021-03-10 13:09:23 +0100

    dernier commit contact pas fonctionnelle

--- app/models/category.js
+++
@@ -1,9 +0,0 @@
-import Model from '@ember-data/model';
-import Model, { hasMany } from '@ember-data/model';
-
-export default class CategoryModel extends Model {
-  @attr name;
-  @hasMany('contacts') contact;
-
-  
-}
--- app/routes/categories.js
+++
@@ -1,4 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class CategoriesRoute extends Route {
-}
