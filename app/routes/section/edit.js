d69ecac9bb730ecc2d70cf8984fdf25b174b0142
ryan <21905274@etu.unicaen.fr>
2021-03-25 22:28:25 +0100

    Affichage du nom des produits d'une section

--- app/routes/section/edit.js
+++ app/routes/section/edit.js
@@ -1,4 +1,26 @@
 import Route from '@ember/routing/route';
+import Abstractroute from '../Abstractroute';
+import RSVP from 'rsvp';
+import { action } from '@ember/object';
 
-export default class SectionEditRoute extends Route {
+export default class SectionEditRoute extends Abstractroute {
+
+  model(params) {
+    if (this.userAuth.user) {
+      return RSVP.hash({
+        section: this.store.findRecord('section', params.id),
+        employee: this.userAuth.user,
+        products: this.store.query('product', {
+           filter: { idSection: params.id },
+         })
+      });
+    }
+  }
+
+  @action
+  board() {
+      this.transitionTo('board');
+  }
+
+
 }
