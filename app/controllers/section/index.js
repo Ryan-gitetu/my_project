b95f0de2f2e342c925878986a11a6a179493f81b
ryan <21905274@etu.unicaen.fr>
2021-04-04 18:47:45 +0200

    modif graphique de section/index+sous route index/add-product,index/add

---
+++ app/controllers/section/index.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+export default class SectionIndexController extends Controller {
+  @tracked description;
+}
